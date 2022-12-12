1789c6b7cb7877735eb536939e40e4b7bf5012e0
Ezechiel <sxedesign@gmail.com>
2022-12-12 22:48:36 +0100

    i will remember this for a while...

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,51 +1,125 @@
-﻿var filePath = args.Count() > 0 && args[0] == "prod" ? "./input.txt" : "./test_input.txt";
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Collections.Immutable;
+using System.Linq;
+var filePath = args.Count() > 0 && args[0] == "prod" ? "./input.txt" : "./test_input.txt";
 var fileContent = File.ReadAllLines(filePath);
 var terrainMap = new TerrainMap(ParseInput(fileContent));
-// foreach(var cell in )
+var startCharacter = 'S';
+var endCharacter = 'E';
+// var edges = ScanMap(terrainMap);
 
+//BFS
+Dictionary<Coordinates, int> PathToDest = new();
+Queue<Coordinates> queToCheck = new();
+var myGoal = terrainMap.gridCells.Single(e => e.Value.letter == endCharacter);
+myGoal.Value.PathToDest = 0;
+myGoal.Value.Visited = true;
+queToCheck.Enqueue(myGoal.Key);
+GridCell resultCell = null;
+while (queToCheck.Count > 0 && resultCell is null)
+{
+    var currCords = queToCheck.Dequeue();
+    var currCell = terrainMap.gridCells[currCords];
+    var pathTodest = currCell.PathToDest + 1;
+    var neighbourCords = terrainMap.GetPossibleCells(currCords);
+    foreach (var cord in neighbourCords)
+    {
+        var nextCell = terrainMap.gridCells[cord];
 
-void ScanMap(TerrainMap map){
-    for(int irows = 0; irows<map.gridcells.Length; irows++){
-        for(int icols = 0; icols< map.gridcells[0].Length; icols++){
-
-        }
+        if (nextCell.Visited) continue;
+        nextCell.Visited = true;
+        nextCell.PathToDest = pathTodest;
+        if (nextCell.letter == 'a') resultCell = nextCell;
+        queToCheck.Enqueue(cord);
     }
 }
+Console.WriteLine(resultCell);
+// Console.WriteLine(terrainMap.gridCells.Single(x => x.Value.letter == startCharacter).Value);
 
+Dictionary<Coordinates, GridCell> ParseInput(string[] input)
+{
 
-GridCell[][] ParseInput(string[] input){
-    return input.Select(line=> line.ToCharArray().Select(currentChar=> new GridCell(currentChar)).ToArray()).ToArray();
-} 
-record TerrainMap(GridCell[][] gridcells){
-    private List<Direction> directions= new (){
+    return (
+    from irow in Enumerable.Range(0, input.Length)
+    from icol in Enumerable.Range(0, input[0].Length)
+    select
+    new KeyValuePair<Coordinates, GridCell>(
+    new Coordinates(irow, icol), new GridCell(input[irow][icol])
+    )).ToDictionary(x => x.Key, x => x.Value);
+    // return input.Select(line => line.ToCharArray().Select(currentChar => new GridCell(currentChar)).ToArray()).ToArray();
+}
+record TerrainMap(Dictionary<Coordinates, GridCell> gridCells)
+{
+    private List<Direction> directions = new(){
         new Direction(0,1,"Right"),
         new Direction(0,-1,"Left"),
         new Direction(1,0,"Up"),
         new Direction(-1,0,"Down"),
     };
-    List<GridCell> GetPossibleNeighbours(GridCell cell){
-        GetNeighours(cell)
-    }
-    List<GridCell> GetNeighours(GridCell cell){
-        var irow = cell.
-        List<GridCell> result = new();
-        foreach(var direction in directions){
-            (int newRow, int newCol)= (irow+direction.drow,icol+direction.dcol);
-            if(!CellExists(newRow,newCol)) continue;
-            result.Add(gridcells[newRow][newCol]);
+
+    // GridCell GetCell(int irow, int icol) => gridCells[new Coordinates(irow, icol)];
+    GridCell? GetCell(Coordinates cord) => CellExists(cord) ? gridCells[cord] : null;
+    public List<Coordinates> GetPossibleCells(Coordinates cords)
+    {
+        var currentCell = GetCell(cords);
+        List<Coordinates> result = new();
+        int icol = cords.icol;
+        int irow = cords.irow;
+        foreach (var direction in directions)
+        {
+            var newCords = cords + direction;
+            if (!CellExists(newCords)) continue;
+            var nextCell = GetCell(newCords);
+            var heightDiff = currentCell.height - nextCell.height;
+            if (heightDiff > 1) continue;
+            result.Add(newCords);
         }
         return result;
     }
-    bool CellExists(int irow, int icol){
-        return irow>=0 && icol>=0 && irow<gridcells.Length && icol< gridcells[0].Length;
+    bool CellExists(Coordinates cords)
+    {
+        return gridCells.ContainsKey(cords);
+    }
+    public void PrintMap()
+    {
+        var maxRows = gridCells.Keys.Max(x => x.irow);
+        var maxCols = gridCells.Keys.Max(x => x.icol);
+        for (int i = 0; i <= maxRows; i++)
+        {
+            var line = "";
+            for (int j = 0; j <= maxCols; j++)
+            {
+                var cords = new Coordinates(i, j);
+                var gc = gridCells[cords];
+                line += $"[{gc.letter}:{gc.height:000}:{gc.PathToDest:00}]\t";
+            }
+            Console.WriteLine(line);
+        }
     }
 
 }
-record GridCell(char letter, int irow, int icol){
-    public int height { get{
-        if(letter =='S') return(int)'a';
-        if(letter =='E') return(int)'z';
-        return (int)letter;
-    } }
+record GridCell(char letter)
+{
+    public int height
+    {
+        get
+        {
+            if (letter == 'S') return (int)'a';
+            if (letter == 'E') return (int)'z';
+            return (int)letter;
+        }
+    }
+    public int PathToDest { get; set; } = 999;
+    public bool Visited { get; set; } = false;
+}
+record Coordinates(int irow, int icol) : IComparable<Coordinates>
+{
+    public int CompareTo(Coordinates? other)
+    {
+        return irow > other.irow && icol > other.icol ? 1 : 0;
+    }
+
+    public static Coordinates operator +(Coordinates cords, Direction direction) =>
+                            new Coordinates(cords.irow + direction.drow, cords.icol + direction.dcol);
 }
 record Direction(int drow, int dcol, string name);
---
+++ 2021/04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+// See https://aka.ms/new-console-template for more information
+var filePath = "./test_input.txt";
+var prodFilePath = "./input.txt";
+var finalCheck = false;
+// finalCheck = true;
+
+
+if (finalCheck)
+{
+    filePath = prodFilePath;
+}
+var input = File.ReadAllText(filePath);
+
+var inputParted = input.Split("\n\n");
+var drawnNumbers = inputParted.First().Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+var boards = ParseInput(inputParted.Skip(1).ToArray());
+Console.Write("asd");
+Board[] ParseInput(string[] input)
+{
+    return input.Select(boardSection =>
+                            boardSection.Split('\n')
+                                .Select(line => line.Split(' ').Select(num => new BingoItem(num)).ToArray()
+                    ).ToArray())
+                    .Select(partial => new Board(partial)).ToArray();
+}
+
+record Board(BingoItem[][] items, bool won = false)
+{
+    private int boardSize = 5;
+    public void check(string number)
+    {
+        this.items.Forea
+    }
+}
+[DebuggerDisplay("{display()}")]
+record BingoItem(string number, bool isChecked = false)
+{
+    public string display() => this.isChecked ? $"[{this.number}]" : $" {this.number} ";
+}
