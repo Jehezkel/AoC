751723c33a94cd55775c6b7a77f3fd825c9516e3
Ezechiel <sxedesign@gmail.com>
2022-12-13 19:57:44 +0100

    everything is a list

--- 13/Program.cs
+++ 13/Program.cs
@@ -4,6 +4,18 @@ using System.Text.RegularExpressions;
 var filePath = args.Count() > 0 && args[0] == "prod" ? "./input.txt" : "./test_input.txt";
 var input = File.ReadAllText(filePath);
 var packetPairsList = ParseInput(input);
+int i = 1;
+int sum = 0;
+foreach (var pair in packetPairsList)
+{
+    Console.WriteLine($"CHecking pair {i}");
+    if (pair.left.content <= pair.right.content)
+    {
+        Console.WriteLine("Fine!");
+        sum += i;
+    }
+    i++;
+}
 Console.WriteLine();
 List<PacketPair> ParseInput(string input)
 {
@@ -15,7 +27,7 @@ List<PacketPair> ParseInput(string input)
         var rightPacket = pair.Split('\n')[1];
         var parsedL = new Packet(ParseList(leftPacket));
         var parsedR = new Packet(ParseList(rightPacket));
-        result.Add(new PacketPair(parsedL,parsedR));
+        result.Add(new PacketPair(parsedL, parsedR));
     }
     return result;
 }
@@ -31,7 +43,7 @@ PacketList ParseList(string bracketsToParse)
         if (currChar == '[')
         {
             var closingBracketIndex = GetClosingIndex(items.Substring(i));
-            var listLength = closingBracketIndex+ 1;
+            var listLength = closingBracketIndex + 1;
             var listToAdd = listLength == 0 ? new PacketList() : ParseList(items.Substring(i, listLength));
             result.value.Add(listToAdd);
             i += listLength;
@@ -51,58 +63,87 @@ int GetClosingIndex(string line)
     // each open bracket delays our closing bracket by one.
     var regex = new Regex(@"(?<open>\[)|(?<close>\])");
     var allBrackets = regex.Matches(line);
-    (int openBracketsCounter,int matchesIterator) = (0,0);
+    (int openBracketsCounter, int matchesIterator) = (0, 0);
     Match myClosingBracket;
     do
     {
         myClosingBracket = allBrackets[matchesIterator];
-        openBracketsCounter = myClosingBracket.Value == "[" ? openBracketsCounter+1 : openBracketsCounter-1;
+        openBracketsCounter = myClosingBracket.Value == "[" ? openBracketsCounter + 1 : openBracketsCounter - 1;
         matchesIterator++;
     } while (openBracketsCounter > 0);
     return myClosingBracket.Index;
 }
-var a = new PacketList();
-var b = new PacketValue(1);
-if(b>=a){
 
-} 
-abstract record PacketElement;
+abstract record PacketElement
+{
+    public static bool operator <=(PacketElement a, PacketElement b)
+    {
+        var aObject = a as PacketList;
+        var bObject = b as PacketList;
+        return aObject <= bObject;
+    }
+    public static bool operator >=(PacketElement a, PacketElement b)
+    {
+        var aObject = a as PacketList;
+        var bObject = b as PacketList;
+        return aObject >= bObject;
+    }
+    public static implicit operator PacketList(PacketElement d) => new(new List<PacketElement>() { d });
+};
 record PacketValue(int value) : PacketElement
 {
-    public static explicit operator PacketList(PacketValue d)=> new (new List<PacketElement>(){d});
-    public static bool operator <=(PacketValue a, PacketValue b){
-        return a.value<=b.value;
+    public static bool operator <=(PacketValue a, PacketValue b)
+    {
+        return a.value <= b.value;
     }
-    public static bool operator >=(PacketValue a, PacketValue b){
-        return  a.value>=b.value;
-    }    
-    public static bool operator >=(PacketValue a, PacketList b){
-        return (PacketList)a>=b;
+    public static bool operator >=(PacketValue a, PacketValue b)
+    {
+        return a.value >= b.value;
     }
-    public static bool operator <=(PacketValue a, PacketList b){
-        return (PacketList)a<=b;
-    }
+
 }
-record PacketList : PacketElement{
-    public PacketList(){}
-    public PacketList(List<PacketElement> value)=>this.value = value;
-    public List<PacketElement> value {get;set;}= new ();
-    public static bool operator <=(PacketList a, PacketList b){
-        if(a.value.Count()>b.value.Count())return false;
-        if(a.value.Count()<b.value.Count())return true;
-        return true;
-    }    
-    public static bool operator >=(PacketList a, PacketList b){
-        if(a.value.Count()<b.value.Count())return false;
-        if(a.value.Count()>b.value.Count())return true;
-        for(int i=0; i<a.value.Count(); i++{
-            if(a.value[i] is PacketValue && b.value[i] is PacketValue){
-                
-            }
-            if(a.value[i]>=b.value[i])continue;
+record PacketList : PacketElement
+{
+    public PacketList() { }
+    public PacketList(List<PacketElement> value) => this.value = value;
+    public List<PacketElement> value { get; set; } = new();
+    public static bool operator >=(PacketValue a, PacketList b)
+    {
+        return (PacketList)a >= b;
+    }
+    public static bool operator <=(PacketValue a, PacketList b)
+    {
+        return (PacketList)a <= b;
+    }
+    public static bool operator <=(PacketList a, PacketList b)
+    {
+        if (a.value.Count() > b.value.Count()) return false;
+        if (a.value.Count() < b.value.Count()) return true;
+        for (int i = 0; i < a.value.Count(); i++)
+        {
+            var aVal = a.value[i];
+            var bVal = b.value[i];
+            if (a.value[i] <= b.value[i]) continue;
+            return false;
+
         }
         return true;
     }
+    public static bool operator >=(PacketList a, PacketList b)
+    {
+        if (a.value.Count() < b.value.Count()) return false;
+        if (a.value.Count() > b.value.Count()) return true;
+        for (int i = 0; i < a.value.Count(); i++)
+        {
+            if (a.value[i] >= b.value[i]) continue;
+            return false;
+        }
+        return true;
+    }
+    // public static explicit operator PacketList(PacketValue d) => new(new List<PacketElement>() { d });
+    public static implicit operator PacketList(PacketValue d) => new(new List<PacketElement>() { d });
+
+
 
 }
 
